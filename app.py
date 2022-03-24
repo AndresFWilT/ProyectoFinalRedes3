1ff3b3f7f92deea84a5ae1d430304e60fe8ba43e
alejandrosd <luisocampo.o.g@gmail.com>
2022-03-24 14:52:30 -0500

    sending email using SMTP on GMAIl server

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
+
 from flask import Flask, render_template
 from flask_mail import Mail, Message
 from config import DevelopmentConfig
 import cx_Oracle
 import json
 import os
+
+import smtplib
+
 
 # Global
 app = Flask(__name__)
@@ -13,18 +17,60 @@ mail = Mail()
 # Default server path
 @app.route('/')
 def init():
+  #PRUEBA DE ENVIO SMTP CON SERVIDOR GMAIL
+  #correo de destino
+  correo_d = 'Destino_prueba@yopmail.com'
+
+  #correo de origen
+  correo_o = 'correo.base.de.datos.uno@gmail.com'
+
+  #definimos parametros del mensaje 
+  mensaje = ('Buen dia, correo enviado usando SMTP ')
+  server = smtplib.SMTP('smtp.gmail.com', 587)
+  server.ehlo()
+  server.starttls()
+
+  msg = 'Subject: {}\n\n{}'.format("Prueba Redes 3", mensaje)  
+
+  #ingreso al correo de gmail
+  server.login('correo.base.de.datos.uno@gmail.com', 'basededatosunoprueba123')  
+  
+  #se envia el correo
+  server.sendmail(correo_o, correo_d, msg) 
+  server.quit()
+ 
+  '''
+  #PRUEBA DE ENVIO SMTP CON SERVIDOR LOCAL
+  #definimos las variables necesarias para el envío del mensaje (remitente, destinatario, asunto y mensaje -en formato HTML-):
+  remitente = "Desde prueba <correo.base.de.datos.uno@gmail.com>" 
+  destinatario = "Hacia prueba <luisocampo.o.g@gmail.com>" 
+  asunto = "E-mal HTML enviado desde Python" 
+  mensaje = """Hola!<br/> <br/> 
+  Este es un <b>e-mail</b> enviando desde <b>Python</b> 
+  """
+  
+  #generamos el e-mail con todos los datos definidos anteriormente:
+  email = """From: %s 
+  To: %s 
+  MIME-Version: 1.0 
+  Content-type: text/html 
+  Subject: %s 
+  %s
+  """ % (remitente, destinatario, asunto, mensaje)
+  
+  # creamos un objeto smtp y realizamos el envío:
+  smtp = smtplib.SMTP('localhost')
+  smtp.sendmail(remitente, destinatario, email)
+  '''
+
   mail.init_app(app)
   return index()
 
 # Path for login
 @app.route('/login')
 def index():
-  return render_template('login.html')
-
-# Path for register
-@app.route('/register')
-def view_register():
-  return render_template('register.html')
+  return 'test'
 
 if __name__ == '__main__':
     app.run(debug=True)
+
