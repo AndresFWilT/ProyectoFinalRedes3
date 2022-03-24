f763cf7a221fff17eb7210b8309607518b8d5420
Andres Felipe Wilches Torres <andresfwilchest@gmail.com>
2022-03-24 13:50:57 -0500

    register.html: added

--- app.py
+++ app.py
@@ -10,16 +10,21 @@ app = Flask(__name__)
 app.config.from_object(DevelopmentConfig)
 mail = Mail()
 
-
 # Default server path
 @app.route('/')
 def init():
   mail.init_app(app)
   return index()
-# Path for index
+
+# Path for login
 @app.route('/login')
 def index():
   return render_template('login.html')
+
+# Path for register
+@app.route('/register')
+def view_register():
+  return render_template('register.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
